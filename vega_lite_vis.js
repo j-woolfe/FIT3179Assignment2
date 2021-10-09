c28b9009b1d112d3117d6e26ad0944d9bae23fd0
Jackson Wain Desktop <jwai0004@student.monash.edu>
2021-10-09 17:19:56 +1100

    Added week 10 homework

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,3 +1,5 @@
 let vg_1 = "astronaut_map.vg.json";
+let vg_2 = "military_status.vg.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {}).catch(console.error);
+vegaEmbed("#military", vg_2).then(function(result) {}).catch(console.error);
