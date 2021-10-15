69ed846efde02a47a005d5f85f28a3b6218959be
Jackson Wain <jwai0004@student.monash.edu>
2021-10-15 18:30:54 +1100

    Added age to js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,7 +1,9 @@
 let vg_1 = "astronaut_map.vg.json";
 let vg_2 = "military_status.vg.json";
 let vg_3 = "gender.vg.json";
+let vg_4 = "age.vg.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {}).catch(console.error);
 vegaEmbed("#military", vg_2).then(function(result) {}).catch(console.error);
 vegaEmbed("#gender", vg_3).then(function(result) {}).catch(console.error);
+vegaEmbed("#age", vg_4).then(function(result) {}).catch(console.error);
