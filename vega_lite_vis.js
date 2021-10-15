d870b7c218a5f42a535509c4993d4912a09bfc09
Jackson Wain <jwai0004@student.monash.edu>
2021-10-15 16:46:13 +1100

    Added gender to js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,7 @@
 let vg_1 = "astronaut_map.vg.json";
 let vg_2 = "military_status.vg.json";
+let vg_3 = "gender.vg.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {}).catch(console.error);
 vegaEmbed("#military", vg_2).then(function(result) {}).catch(console.error);
+vegaEmbed("#gender", vg_3).then(function(result) {}).catch(console.error);
